e221717d555fc8f584737495da5cf3db5bd47cb3
Vampi <47399571+Vampitech@users.noreply.github.com>
2020-06-07 03:57:56 -0500

    Reboot to NeXT Declare

--- source/utils/util.h
+++ source/utils/util.h
@@ -69,6 +69,7 @@ void usleep(u32 us);
 void msleep(u32 ms);
 void panic(u32 val);
 void reboot_normal();
+void reboot_next();
 void reboot_rcm();
 void power_off();
 void exec_cfg(u32 *base, const cfg_op_t *ops, u32 num_ops);
